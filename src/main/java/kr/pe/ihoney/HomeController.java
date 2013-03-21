757594c0b3c1c3eeb9efcb10795ad39c2ebc9a8b
honeymon <kim.jiheon@softwareinlife.com>
2013-03-21 13:54:19 +0900

    #1 MVC 에서 @PathVariable에 enum 타입 활용하기

--- src/main/java/kr/pe/ihoney/HomeController.java
+++ src/main/java/kr/pe/ihoney/HomeController.java
@@ -4,10 +4,13 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import kr.pe.ihoney.type.CodeDescriptionOrderEnum;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,6 +36,36 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
+		return "home";
+	}
+	
+	@RequestMapping(value="/enum/{enumCode}", method=RequestMethod.GET)
+	public String enumPathVariable(@PathVariable("enumCode") CodeDescriptionOrderEnum codeDescriptionOrderEnum, Model model) {
+		String message = "";
+		switch (codeDescriptionOrderEnum) {
+		case FIRST:
+			message = "First is 1st";
+			break;
+		case SECOND:
+			message = "SECOND is 2nd";
+			break;
+		case THIRD:
+			message = "THIRD is 3rd";
+			break;
+		case FOURTH:
+			message = "FOURTH is 4th";
+			break;
+		case FIFTH:
+			message = "FIFTH is 5th";
+			break;
+
+		default:
+			message = "Unknown!!";
+			break;
+		}
+		
+		model.addAttribute("enumType", codeDescriptionOrderEnum.getCode());
+		model.addAttribute("message", message );
 		return "home";
 	}
 	
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -10,5 +10,7 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+<h1> Enum타입 : ${enumType}</h1>
+<h2> Message : ${message}</h2>
 </body>
 </html>
